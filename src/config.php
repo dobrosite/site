fa7b9585eedf3dd981bdaae420c673b032a26d87
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-02-14 22:21:23 +0300

    Настройки, разделяемые между исполняемыми файлами приложения

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * site — автоматизация управления сайтами.
+ *
+ * Разделяемые настройки.
+ *
+ * @copyright 2017, Михаил Красильников <m.krasilnikov@yandex.ru>
+ * @author    Михаил Красильников <m.krasilnikov@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+declare(strict_types=1);
+
+// Полное имя к файлу скомпилированного контейнера зависимостей.
+define('DIC_DUMP_FILE', dirname(__DIR__) . '/resources/dic.compiled.php');
