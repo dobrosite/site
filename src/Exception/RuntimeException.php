e71c2e5f3e1e8ac23ed7dc9d2469cab750e60153
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-02-14 22:32:05 +0300

    Основное исключение времени исполнения

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * site — автоматизация управления сайтами.
+ *
+ * @copyright 2017, Михаил Красильников <m.krasilnikov@yandex.ru>
+ * @author    Михаил Красильников <m.krasilnikov@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+declare(strict_types=1);
+
+namespace DobroSite\Site\Exception;
+
+/**
+ * Ошибка времени исполнения.
+ */
+class RuntimeException extends \RuntimeException
+{
+}
